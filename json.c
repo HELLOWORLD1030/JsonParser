bdbab52b8048e496b769afe49522745ffaf12706
HELLOWORLD <zzg373692503@hotmail.com>
2022-07-07 23:45:38 +0800

    Init repo

---
+++ json.c
@@ -0,0 +1,17 @@
+//
+// Created by HELLO_WORLD on 2022-07-05.
+//
+#include "json.h"
+#include <string.h>
+cJSON * cJSON_Parse(const char *value){
+    cJSON * cJson=0;
+    return cJson;
+}
+void Check(const char *value){
+    if(value==NULL){
+       ;
+    }
+    char* temp=value;
+    strtok(temp,",");
+
+}
